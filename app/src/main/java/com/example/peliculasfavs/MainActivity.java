a98e93b56a62ceb91d8fe3f0091e0ad3589900d5
ELnino <apuma2002@gmail.com>
2025-02-04 20:55:23 +0100

    Funcionalidades

--- app/src/main/java/com/example/peliculasfavs/MainActivity.java
+++ app/src/main/java/com/example/peliculasfavs/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.peliculasfavs;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    public EditText txtAgregar, txtCargar;
+    public Button btnAgregar, btnCargar;
+    public SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,28 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+            });
+        txtAgregar = findViewById(R.id.txtAgregar);
+        txtCargar = findViewById(R.id.txtCargar);
+        btnAgregar = findViewById(R.id.btnAgregar);
+        btnCargar = findViewById(R.id.btnCargar);
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String key = txtAgregar.getText().toString();
+                String value = txtCargar.getText().toString();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(key, value);
+                editor.apply();
+            }
+        });
+        btnCargar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String key = txtAgregar.getText().toString();
+                String value = sharedPreferences.getString(key, "No encontrado");
+                txtCargar.setText(value);
+            }
         });
     }
-}git a
+}
